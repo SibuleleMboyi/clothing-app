1f5f8af8f8cf9dd79c00046293008059d4f4a877
SibuleleMboyi <smboyi2016@gmail.com>
2022-07-16 17:43:19 +0200

    creating a home route

--- src/App.js
+++ src/App.js
@@ -1,43 +1,12 @@
-import CategoryList from "./components/category-list/category-list.component";
+import {Routes, Route} from 'react-router-dom';
 
+import Home from './routes/home/home.component';
 
 const App = () => {
-
-  const categories =[
-    {
-      "id": 1,
-      "title": "Hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-
-    {
-      "id": 2,
-      "title": "Jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-
-    {
-      "id": 3,
-      "title": "Sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-
-    {
-      "id": 4,
-      "title": "Women's",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womes.png"
-    },
-
-    {
-      "id": 5,
-      "title": "Men's",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
-
-
   return (
-    <CategoryList categories = {categories}></CategoryList>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 };
 
