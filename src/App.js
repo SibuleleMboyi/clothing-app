db1f0354b0c1ce934f39fc21c5797b20229c438b
SibuleleMboyi <smboyi2016@gmail.com>
2022-07-16 17:44:31 +0200

    creating a persistent component to simulate a navigation bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, Outlet} from 'react-router-dom';
 
 import Home from './routes/home/home.component';
 
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1> I am the navigation bar</h1>
+      </div> <Outlet />
+    </div>
+  );
+};
+
+const Shop = () => {
+  return <h1> I am the shop page</h1>;
+};
 const App = () => {
   return (
+    // in the "home" route, we could pass the property, "path = "
+    // But then we would always need to open the home page at "http://localhost:3000/home"
+    // Instead we want to open home page at the root of the application.
+    // we replace the "path = " property with "index". It defaults to the 1st route provided. 
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
